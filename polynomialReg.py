68450c5cfa7e231c26e53ea63e7078d93bb0320d
juliomtz00 <julio.mtz00@hotmail.com>
2023-10-05 14:01:33 -0600

    UPDATE

--- polynomialReg.py
+++ polynomialReg.py
@@ -35,5 +35,5 @@ equation = sp.Eq(matrix * vector, final)
 solution = sp.solve(equation,[a,b,c])
 print(solution)
 
-SSE = [np.power(y[i] - solution['a']*x2[i] - solution['b']*x[i] - solution['c'],2)for i in range(len(x))]
+SSE = [np.power(y[i] - solution[a]*x2[i] - solution[b]*x[i] - solution[c],2)for i in range(len(x))]
 print(SSE)
