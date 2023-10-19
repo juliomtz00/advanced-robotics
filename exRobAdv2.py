2dc638c692b53885117bf9dba0f9bbde9eff7a0c
juliomtz00 <julio.mtz00@hotmail.com>
2023-10-19 10:16:44 -0600

    updates to the file

--- exRobAdv2.py
+++ exRobAdv2.py
@@ -35,16 +35,22 @@ params1 = norm.fit(Data1[1])
 params2 = norm.fit(Data2[1])
 params3 = norm.fit(Data3[1])
 
+# Create the list to save the max percentage names
 percentage = []
 type = ["Asphalt","Earth","Grass"]
 
+# Loop to check all the points inside the list from the column intensity
 for point in newData['intensity']:
+
+    # Check points with the generated equations accordingly
     prob1 = (1/(sd1*np.sqrt(2*np.pi)))*np.exp(-(1/2)*((point-mean1)/sd1)**2)
     prob2 = 1/(sd2*sp.sqrt(2*np.pi))*sp.exp(-(1/2)*((point-mean2)/sd1)**2)
     prob3 = 1/(sd3*sp.sqrt(2*np.pi))*sp.exp(-(1/2)*((point-mean3)/sd1)**2)
 
+    # Obtain the max value from all three and save it in a variable
     maxProb = max(prob1,prob2,prob3)
     
+    # Check which of the obtained values and save the corresponding name to the variable percentage list.
     if maxProb == prob1:
         percentage.append(type[0])
     elif maxProb == prob2: 
@@ -52,18 +58,23 @@ for point in newData['intensity']:
     else:
         percentage.append(type[2])
 
+# Calculate the amount of percentage that was obtained for each type
 asphaltPercentage = sum(1 for x in percentage if x == "Asphalt")/len(newData['intensity'])*100
 earthPercentage = sum(1 for x in percentage if x == "Earth")/len(newData['intensity'])*100
 grassPercentage = sum(1 for x in percentage if x == "Grass")/len(newData['intensity'])*100
 
+# Calculate the max value from the percentages
 result = max(asphaltPercentage,earthPercentage,grassPercentage)
+
+# Check which one corresponds to the obtained percentages and save the name as observedTerrain
 if result == asphaltPercentage:
-    observedTerrain = "Asphalto"
+    observedTerrain = "Asfalto"
 elif result == earthPercentage: 
     observedTerrain = "Tierra"
 else:
     observedTerrain = "Pasto"
 
+# Print requested values
 print(f"Cantidad de puntos analizados: {len(newData['intensity'])}")
 print(f"Porcentaje de asphalto: {asphaltPercentage}")
 print(f"Porcentaje de tierra: {earthPercentage}")
