ecec8776d5f659d86b099544981ab31549c2a2ac
juliomtz00 <julio.mtz00@hotmail.com>
2023-10-23 16:04:53 -0600

    Update cameraEnabling.py

--- cameraEnabling.py
+++ cameraEnabling.py
@@ -26,11 +26,15 @@ import argparse
 # Parse user's arguments from command line
 parser = argparse.ArgumentParser()
 parser.add_argument('--cam_index', type=int, default=1,help="Index value for the Camera")
+parser.add_argument("--path_to_saved_images",
+                    type=str,
+                    default='captured-images',
+                    help='Folder where the captured images will be stored')
 #parser.add_argument('--cal_file', type=str, help="Name of the calibration file")
 args = parser.parse_args()
     
 # Create a new window
-cv2.namedWindow("Current frame", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Camera Frame", cv2.WINDOW_NORMAL)
 
 # Create a video capture object for video streaming
 camera_index = args.cam_index
@@ -42,6 +46,7 @@ image_height = float(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print("IMAGE MEASUREMENTS: height: "+str(image_height)+", width: "+str(image_width))
 
 # If camera opens correctly
+i = 0
 while(video_capture.isOpened()):
     #Get the current frame and pass it on to 'frame''
     #If the current frame cannot be captured, ret = 0
@@ -52,7 +57,7 @@ while(video_capture.isOpened()):
         print("Frame missed!")
 
      # If so, it is then visualised
-    cv2.imshow("Current frame", frame)
+    cv2.imshow("Camera Frame", frame)
     
     # Retrieve the pressed key
     key = cv2.waitKey(1)
@@ -60,13 +65,16 @@ while(video_capture.isOpened()):
     # If the pressed key was 's''
     #the current image is saved into disk
     if key == ord('s'):
-        cv2.imwrite("current_frame.png", frame)
+        path_and_image_name = str(args.path_to_saved_images)+'frame'+"-"+str(f"{i:04d}.png")
+        print('Captured image saved in: ' + path_and_image_name)
+        cv2.imwrite(path_and_image_name, frame)
+        i += 1
     
     # If the pressed key was  'd''
     # the program finishes
     elif key == ord("q"):
         break
-
+        
 #Close video capture object
 video_capture.release()
 
