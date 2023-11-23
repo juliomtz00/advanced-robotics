a91d710597f7cb3ee4ffac91cc4dc8e318fc3afa
juliomtz00 <julio.mtz00@hotmail.com>
2023-11-23 00:32:29 -0600

    robotics lab updates

--- getAngleCamera.py
+++ getAngleCamera.py
@@ -1,11 +1,9 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-def computeAngle(frame,mtx,dist,angle_values):
+def computeAngle(frame,mtx,dist):
     # Get size
     h,  w = frame.shape[:2]
-
     # Get optimal new camera
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 0, (w, h))
 
@@ -52,54 +50,51 @@ def computeAngle(frame,mtx,dist,angle_values):
     
         # Iterate through the contours and filter out small ones
         filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 100]
-        print("Number of filtered contours detected:", len(filtered_contours))
+        #print("Number of filtered contours detected:", len(filtered_contours))
         
         fIteration = len(filtered_contours)
-        filtered_last = filtered_contours[fIteration-1]
-        approx = cv2.approxPolyDP(filtered_last, 0.02*cv2.arcLength(filtered_last, True), True)
-        
-        # Check if there are enough points to fit an ellipse
-        if len(filtered_last) == 4: 
-            average_point = np.mean(filtered_last[:, 0], axis=0, dtype=np.int32)
-            filtered_last = np.vstack([filtered_last[:, 0], average_point.reshape(1, 2)])
+        if fIteration > 1:
+            filtered_last = filtered_contours[fIteration-1]            
+            # Check if there are enough points to fit an ellipse
+            if len(filtered_last) == 4: 
+                average_point = np.mean(filtered_last[:, 0], axis=0, dtype=np.int32)
+                filtered_last = np.vstack([filtered_last[:, 0], average_point.reshape(1, 2)])
 
-        if len(filtered_last) >= 5:
-            # compute the center of mass of the triangle
-            originalImage = cv2.drawContours(dst, [filtered_last], -1, (0,255,255), 3)
-            # compute the center of mass of the triangle
-            M = cv2.moments(filtered_last)
-            '''for num in cnt:
-                print(num)
-            #print(M)'''
-            if M['m00'] != 0.0:
-                centroidU = int(M['m10']/M['m00'])
-                centroidV = int(M['m01']/M['m00'])
-                # fit an ellipse to the largest contour to find its orientation
-                ellipsoidShape = cv2.fitEllipse(filtered_last)
-                # ellipsoidShape[0]  ->  (centroideCoordX, centroideCoordY)
-                # ellipsoidShape[1]  ->  (centroideWidth, centroideHeight)
-                # ellipsoidShape[2]  ->  Angulo que genera la elipse, los ejes no corresponden a los de un eje cordenado
-                #                        normal, su X corresponde al eje Y (normal), y, su Y corresponde al eje X (normal)
-                angle = ellipsoidShape[2]
-                # -------------------------------------------------------------
-                # -------------------------------------------------------------
-                angle_values.append(angle)
-                if len(angle_values) > 10:
-                    angle_values.pop(0)
-                
-                average_angle = np.sum(angle_values)/len(angle_values)
-                cv2.circle(dst, (centroidU, centroidV), 5, (0, 0, 255), -1)   # RED
-                #cv2.ellipse(dst, ellipsoidShape, (0, 255, 0), 2)              # GREEN
-                print(f'Centroid (x,y): ({centroidU}, {centroidV})')
-                print(f'Orientation angle: {angle} degrees')
-                # -------------------------------------------------------------
-                # AGREGADO
-                # -------------------------------------------------------------
-                cv2.drawContours(dst, [filtered_last], -1, (0, 255, 255), 3)         # YELLOW
-                # -------------------------------------------------------------
-                 # If so, it is then visualised
-                #cv2.putText(dst, f'Angle:{round(average_angle,3)}', (centroidU, centroidV), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+            if len(filtered_last) >= 5:
+                # compute the center of mass of the triangle
+                originalImage = cv2.drawContours(dst, [filtered_last], -1, (0,255,255), 3)
+                # compute the center of mass of the triangle
+                M = cv2.moments(filtered_last)
+                '''for num in cnt:
+                    print(num)
+                #print(M)'''
+                if M['m00'] != 0.0:
+                    centroidU = int(M['m10']/M['m00'])
+                    centroidV = int(M['m01']/M['m00'])
+                    # fit an ellipse to the largest contour to find its orientation
+                    ellipsoidShape = cv2.fitEllipse(filtered_last)
+                    # ellipsoidShape[0]  ->  (centroideCoordX, centroideCoordY)
+                    # ellipsoidShape[1]  ->  (centroideWidth, centroideHeight)
+                    # ellipsoidShape[2]  ->  Angulo que genera la elipse, los ejes no corresponden a los de un eje cordenado
+                    #                        normal, su X corresponde al eje Y (normal), y, su Y corresponde al eje X (normal)
+                    angle = ellipsoidShape[2]
+                    # -------------------------------------------------------------
+                    # -------------------------------------------------------------
+                    
+                    cv2.circle(dst, (centroidU, centroidV), 5, (0, 0, 255), -1)   # RED
+                    #cv2.ellipse(dst, ellipsoidShape, (0, 255, 0), 2)              # GREEN
+                    #print(f'Centroid (x,y): ({centroidU}, {centroidV})')
+                    #print(f'Orientation angle: {angle} degrees')
+                    # -------------------------------------------------------------
+                    # AGREGADO
+                    # -------------------------------------------------------------
+                    cv2.drawContours(dst, [filtered_last], -1, (0, 255, 255), 3)         # YELLOW
+                    # -------------------------------------------------------------
+                    # If so, it is then visualised
+                    #cv2.putText(dst, f'Angle:{round(average_angle,3)}', (centroidU, centroidV), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
 
-                return dst, angle_values, angle
-            else:
-                print('Error')
+                    return dst, angle, centroidU, centroidV
+                else:
+                    print('Error')
+        else:
+            return dst,360,w+10,h+10
--- mainServer.py
+++ mainServer.py
@@ -19,6 +19,12 @@ c_n = []
 doTurn = 0
 doForward = 0
 
+previousAngle = 0
+previousCoordU = 0
+previousCoordV = 0
+
+getOrigin = 0
+
 signAng = 0
 
 angGiro = 0
@@ -26,22 +32,21 @@ angN = 0
 
 dist = 0
 
-
 # ------------------------------------
 # VARIABLES PARA LA VERSION CON CAMARA
 # ------------------------------------
-coordU = [] # coordsU[0] seria el origen en X en pixeles
-coordV = [] # coordsV[0] seria el origen en Y en pixeles
-coordAng = [] # coordsV[0] seria el origen en Y
+coordU = []  # coordsU[0] seria el origen en X en pixeles
+coordV = []  # coordsV[0] seria el origen en Y en pixeles
+coordAng = []  # coordsV[0] seria el origen en Y
 # startU = 0 # Coordenada origen en X
 # startV = 0 # Coordenada origen en Y
 # startAng = 0
 # prevU = 0
 # prevV = 0
 # prevAng = 0
-centrU = 0 # Coordenada que se recibe de la camara, llegar al objetivo
-centrV = 0 # Coordenada que se recibe de la camara, llegar al objetivo
-centrAng = 0 # Angulo que se recibe de la camara, llegar al objetivo
+centrU = 0  # Coordenada que se recibe de la camara, llegar al objetivo
+centrV = 0  # Coordenada que se recibe de la camara, llegar al objetivo
+centrAng = 0  # Angulo que se recibe de la camara, llegar al objetivo
 # ------------------------------------
 
 
@@ -54,24 +59,26 @@ clientTCP.connect(socketInfo)
 print('Conectado al Servidor: ', str(socketInfo))
 
 # Para el cv2 el cuadrante que se transforma sera la del eje Y
-angle_values = []
+angle_values = list()
 
 # Reading an image using OpenCV, extracting its height and width, and converting it to grayscale.
 # Create a new window
-cv2.namedWindow("Camera Frame", cv2.WINDOW_NORMAL)
+#cv2.namedWindow("Camera Frame", cv2.WINDOW_NORMAL)
 
 # Create a video capture object for video streaming
-camera_index = 0 # this is the camera index
+camera_index = 0  # this is the camera index
 video_capture = cv2.VideoCapture(camera_index)
-video_capture.set(cv2.CAP_PROP_AUTOFOCUS, 0) # turn the autofocus off
+video_capture.set(cv2.CAP_PROP_AUTOFOCUS, 0)  # turn the autofocus off
 
 # Calculate the image measurements of the camera through the functions of the opencv library.
 image_width = float(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 image_height = float(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print("IMAGE MEASUREMENTS: height: "+str(image_height)+", width: "+str(image_width))
+print(image_height, image_width)
+print("IMAGE MEASUREMENTS: height: " + str(image_height) + ", width: " + str(image_width))
 
 # Open the calibration file
-calibration_file = open("rob-lab-calib/calibration-parameters.txt", 'r')
+calibration_file = open("/Users/juliomtz00/Documents/GitHub/advanced-robotics/rob-lab-calib/calibration-parameters.txt",
+                        'r')
 
 # Initialize variables to store mtx and dist
 mtx = None
@@ -100,9 +107,10 @@ calibration_file.close()
 i = 0
 
 
-def matrixHTM():
-    heightScreen = 0
-    # widthScreen = 0
+def matrixHTM(centrU, centrV, centrAng):
+    #     dst, centrAng, centrU, centrV = computeAngle(frame,mtx,dist)
+    # #     heightScreen = 0
+    # #     # widthScreen = 0
 
     # Definir la matriz de rotación para 90 grados en el plano XY
     theta = np.radians(90)
@@ -111,20 +119,26 @@ def matrixHTM():
 
     # Definir el vector o la matriz que deseas rotar
     # Por ejemplo, un vector [x, y] o una matriz [[a, b], [c, d]]
-#     vector_or_matrix = np.array([5,7])  # Cambia esto según tus necesidades
-#     LLAMAR A LA FUNCION DE LA CAMARA QUE REGRESE LOS SIGUIENTES
-#     centrU, centrV, centrAng =
+    #     vector_or_matrix = np.array([5,7])  # Cambia esto según tus necesidades
+    #     LLAMAR A LA FUNCION DE LA CAMARA QUE REGRESE LOS SIGUIENTES
+    #     centrU, centrV, centrAng =
     vector_or_matrix = np.array([centrU, centrV])  # Cambia esto según tus necesidades
 
     # Realizar la multiplicación matricial para la rotación
     rotated_vector_or_matrix = np.dot(rotation_matrix, vector_or_matrix)
 
-#     # Imprimir el resultado
-#     print("Vector o matriz original:", vector_or_matrix)
-#     print("Vector o matriz rotado 90 grados:", rotated_vector_or_matrix)
-    coordU.append(rotated_vector_or_matrix[0])
-    coordV.append(rotated_vector_or_matrix[1])
-    coordAng.append(centrAng)
+    #     # Imprimir el resultado
+    #     print("Vector o matriz original:", vector_or_matrix)
+    #     print("Vector o matriz rotado 90 grados:", rotated_vector_or_matrix)
+    try:
+        if coordU[-1]:
+            pass
+    except:
+        coordU.append(rotated_vector_or_matrix[0])
+        coordV.append(image_height - rotated_vector_or_matrix[1])
+        coordAng.append(centrAng)
+    #     return centrU, centrV, centrAng
+    return rotated_vector_or_matrix[0], (image_height - rotated_vector_or_matrix[1]), centrAng
 
 
 # ESTA FUNCION FUE REESTRUCTURADA
@@ -140,7 +154,7 @@ def uncompressInput(input_string):  # Ej. coordenadas multiples -> "(10,0); (-2,
             # Remove parentheses and split by comma to get x and y
             x, y, z = item.strip('( )').split(',')
         except:
-            indexPop.append(nIndex)
+            #             indexPop.append(nIndex)
             try:
                 # Remove parentheses and split by comma to get x and y
                 x, y = item.strip('( )').split(',')
@@ -150,6 +164,7 @@ def uncompressInput(input_string):  # Ej. coordenadas multiples -> "(10,0); (-2,
                 except:
                     z = ''
             except:
+                indexPop.append(nIndex)
                 continue
         if len(x) > 0 and len(y) > 0:
             try:
@@ -167,9 +182,16 @@ def uncompressInput(input_string):  # Ej. coordenadas multiples -> "(10,0); (-2,
                 z_n.append(float(z))
             else:
                 z_n.append(0)
-            ang_n.append(math.degrees(math.atan2(float(y),float(x))))
+            '''ang_n.append(math.degrees(math.atan2(float(y), float(x))))
             # c_values es la distancia de la diagonal
-            c_n.append(np.sqrt(float(x)**2 + float(y)**2))
+            c_n.append(np.sqrt(float(x) ** 2 + float(y) ** 2))'''
+            try:
+                # ang_n.append(math.degrees(math.atan2(float(y_n[-1] - y_n[-2]), float(x_n[-1] - x_n[-2]))))
+                ang_n.append(math.degrees(math.atan2(float(y_n[-1] - y_n[-2]), float(x_n[-1] - x_n[-2]))) - ang_n[-1])
+                c_n.append(np.sqrt(float(x_n[-1] - x_n[-2]) ** 2 + float(y_n[-1] - y_n[-2]) ** 2))
+            except:
+                ang_n.append(math.degrees(math.atan2(float(y_n[-1]), float(x_n[-1]))))
+                c_n.append(np.sqrt(float(x_n[-1]) ** 2 + float(y_n[-1]) ** 2))
         else:
             indexPop.append(nIndex)
             continue
@@ -194,74 +216,112 @@ def sendRaspberry(ang_n, c_n, coordU, coordV, coordAng):
     # REESTRUCTURACION DE GIRO Y TRAYECTO AL MISMO NIVEL
     # --------------------------------------------------
     print()
+    #     doOne = 0
     signAng = 1
-    sendRasp = 0
+    #     sendRasp = 0
+    sendRasp = 4
     condStop = 0
     for cycle in range(2):
-#         print('\n')
-        if cycle == 0:
-            condStop = ang_n
-            if condStop < 0: # turnClockwise
-                signAng = -1
-                sendRasp = 1
-            elif condStop > 0: # turnCounterClockwise
-                signAng = 1
-                sendRasp = 2
-            # ------------------------------------------
-            try:
-                condStop = ang_n - coordAng[-1]
-                if condStop > 0: # turnClockwise
-                    sendRasp = 1
-                elif condStop < 0: # turnCounterClockwise
-                    sendRasp = 2
-            except:
-                pass
-#             condStop = ang_n
-#             if condStop < 0: # turnClockwise
-#                 signAng = -1
-#                 sendRasp = 1
-#             elif condStop > 0: # turnCounterClockwise
-#                 signAng = 1
-#                 sendRasp = 2
-#             condStop = ang_n * signAng
-            textCond = 'Angulo de giro'
-            textCondCompl = '°'
-        elif cycle == 1:
-            condStop = c_n
-            if c_n > 0:
-                sendRasp = 3
-            textCond = 'Distancia'
-            textCondCompl = 'px'
-        cmd = str(sendRasp)
-        cmdEncoded = cmd.encode('utf-8')
-        clientTCP.send(cmdEncoded)
+        doOneTime = 0
         while sendRasp != 0:
-#            LLAMAR A LA FUNCION DE LA CAMARA QUE REGRESE LOS SIGUIENTES
-#             centrU, centrV, angCamera =
-            #Get the current frame and pass it on to 'frame''
-            #If the current frame cannot be captured, ret = 0
-            ret,frame = video_capture.read()
+            #            LLAMAR A LA FUNCION DE LA CAMARA QUE REGRESE LOS SIGUIENTES
+            #             centrU, centrV, angCamera =
+            # Get the current frame and pass it on to 'frame''
+            # If the current frame cannot be captured, ret = 0
+            ret, frame = video_capture.read()
 
             # If ret=0
             if not ret:
                 print("Frame missed!")
 
-            dst, angle_values, angle = computeAngle(frame,mtx,dist,angle_values)
+            dst, angle, centrU, centrV = computeAngle(frame, mtx, dist)
+            print(angle, centrU, centrV)
+            '''
+            if angle == 360:
+                angle = previousAngle
+            previousAngle = angle
 
-            cv2.imshow("Camera Frame", dst)
+            if centrU > image_width or centrV > image_height:
+                centrU = previousCoordU
+                centrV = previousCoordV
+
+            previousCoordU = centrU
+            previousCoordV = centrV
+            '''
+            # --------------------------------------------------
+            # --------------------------------------------------
+            if (centrU > image_width) or (centrV > image_height) or (angle >= 360):
+                sendRasp = 0
+                print('NO TRIANGLE DETECTED')
+                break
+            # centrU, centrV, angle = matrixHTM(centrU, centrV, centrAng)
+            angle_values.append(angle)
+            if len(angle_values) > 10:
+                angle_values.pop(0)
+
+            average_angle = np.sum(angle_values) / len(angle_values)
+
+            centrU, centrV, null = matrixHTM(centrU, centrV, average_angle)
+
+            # cv2.imshow("Camera Frame", dst)
+            # --------------------------------------------------
+            if doOneTime == 0:
+                if cycle == 0:
+                    condStop = ang_n - coordAng[-1]
+                    if condStop < 0:  # turnClockwise
+                        signAng = -1
+                        sendRasp = 1
+                    elif condStop > 0:  # turnCounterClockwise
+                        signAng = 1
+                        sendRasp = 2
+                    textCond = 'Angulo de giro'
+                    textCondCompl = '°'
+                elif cycle == 1:
+                    condStop = c_n
+                    if c_n > 0:
+                        sendRasp = 3
+                    textCond = 'Distancia'
+                    textCondCompl = 'px'
+                doOneTime += 1
+            '''if doOneTime == 0:
+                if cycle == 0:
+                    try:
+                        condStop = ang_n - coordAng[-1]
+                        if condStop > 0:  # turnClockwise
+                            sendRasp = 1
+                        elif condStop < 0:  # turnCounterClockwise
+                            sendRasp = 2
+                    except:
+                        #                     pass
+                        condStop = ang_n
+                        if condStop < 0:  # turnClockwise
+                            signAng = -1
+                            sendRasp = 1
+                        elif condStop > 0:  # turnCounterClockwise
+                            signAng = 1
+                            sendRasp = 2
+                    textCond = 'Angulo de giro'
+                    textCondCompl = '°'
+                elif cycle == 1:
+                    condStop = c_n
+                    if c_n > 0:
+                        sendRasp = 3
+                    textCond = 'Distancia'
+                    textCondCompl = 'px'
+                doOneTime += 1'''
 
             match sendRasp:
                 case 3:
-                    cond = np.sqrt(float(centrU - coordU[-1])**2 + float(centrV - coordV[-1])**2)
+                    cond = np.sqrt(float(centrU - coordU[-1]) ** 2 + float(centrV - coordV[-1]) ** 2)
                 case other:
-                    cond = coordAng[-1]
+                    cond = average_angle - coordAng[-1]
             # --------------------------------------------------
             # --------------------------------------------------
             # YA NO DEBE RECIBIR DE LA RASPBERRY
             # --------------------------------------------------
-#             recvData, address = clientTCP.recvfrom(1024)
-#             dataPrint = recvData.decode('utf-8')
-#             cond = int(dataPrint)
+            #             recvData, address = clientTCP.recvfrom(1024)
+            #             dataPrint = recvData.decode('utf-8')
+            #             cond = int(dataPrint)
             # --------------------------------------------------
             # --------------------------------------------------
             print(textCond, ': ', str(cond), textCondCompl)
@@ -271,31 +331,45 @@ def sendRaspberry(ang_n, c_n, coordU, coordV, coordAng):
             cmdEncoded = cmd.encode('utf-8')
             clientTCP.send(cmdEncoded)
         if sendRasp == 0:
-            print() # print('\n')
-            print('Llegaste al objetivo. ', textCond, ' final: ', str(condStop * signAng), textCondCompl)
+            print()  # print('\n')
+            try:
+                print('Llegaste al objetivo. ', textCond, ' final: ', str(condStop * signAng), textCondCompl)
+            except:
+                pass
+            print()
+        print()
         time.sleep(1)
     # --------------------------------------------------
+    try:
+        if coordU[-1]:
+            coordU.append(centrU)
+            coordV.append(centrV)
+            coordAng.append(average_angle)
+    except:
+        pass
     time.sleep(1)
     return
 
 
 while True:
     print('\n')
-    userInputGame = input('Que tipo de juego: \n 1) Single-Waypoint Navigation. \n 2) Multiple-Waypoints Navigation. \n')
-# ------------------------------------------------------------------ #
-# ----------------------PRIMER TIPO DE JUEGO------------------------ # 
+    userInputGame = input(
+        'Que tipo de juego: \n 1) Single-Waypoint Navigation. \n 2) Multiple-Waypoints Navigation. \n')
+    # ------------------------------------------------------------------ #
+    # ----------------------PRIMER TIPO DE JUEGO------------------------ #
     if userInputGame == "1":
         inGame = 1
-# ------------------------------------------------------------------ #
-# ----------------------SEGUNDO TIPO DE JUEGO----------------------- # 
+    # ------------------------------------------------------------------ #
+    # ----------------------SEGUNDO TIPO DE JUEGO----------------------- #
     elif userInputGame == "2":
         inGame = 1
     else:
         inGame = 0
         print()
         print("Vuelve a intentarlo")
-    
+
     while inGame:
+        getOrigin = 0
         # ------------------------
         # Reinicia las coordenadas
         # ------------------------
@@ -317,15 +391,14 @@ while True:
         print('z_n: ', str(z_n))
         print('ang_n: ', str(ang_n))
         print('c_n: ', str(c_n))
-        
+
         # Guarda las coordenadas iniciales del carrito de acuerdo con un eje coordenado normal
-        matrixHTM()
-        
+
         if len(coord_n[-1]) > 0:
             for nCoord in range(len(coord_n[-1])):
-                #sendRaspberry(nCoord, x_n, y_n, z_n, ang_n, c_n):
+                # sendRaspberry(nCoord, x_n, y_n, z_n, ang_n, c_n):
                 sendRaspberry(ang_n[nCoord], c_n[nCoord], coordU, coordV, coordAng)
-#                 prevU, prevV, prevAng = sendRaspberry(x_n[nCoord], y_n[nCoord], ang_n[nCoord], c_n[nCoord], coordU, coordV):
+            #                 prevU, prevV, prevAng = sendRaspberry(x_n[nCoord], y_n[nCoord], ang_n[nCoord], c_n[nCoord], coordU, coordV):
             inGame = 0
             break
         else:
